786e2e33dc0c8b68a51d6b2de40559f73602f930
Vitaly Melnikov <vmelnikov@bk.ru>
2018-10-23 20:45:16 +0300

    Add RouteCollection in app.php

---
+++ src/app.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+use Symfony\Component\Routing;
+
+$routes = new Routing\RouteCollection();
+$routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
+$routes->add('bye', new Routing\Route('/bye'));
+
+return $routes;
--- src/pages/bye.php
+++ src/pages/bye.php
@@ -1,6 +1,1 @@
-<?php declare(strict_types=1);
-
-require_once __DIR__ . '/../../init.php';
-
-$response->setContent('Goodbye!');
-$response->send();
+Goodbye!
