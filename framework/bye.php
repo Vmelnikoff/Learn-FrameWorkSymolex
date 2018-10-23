4fa8eb0e4bafc2bad344b729721f134b72b6b99b
Vitaly Melnikov <vmelnikov@bk.ru>
2018-10-23 16:51:10 +0300

    Add bye.php

--- framework/bye.php
+++ framework/bye.php
@@ -7,8 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
 
-$name = $request->get('name', 'World');
-
-$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+$response = new Response('Goodbye!');
 
 $response->send();
