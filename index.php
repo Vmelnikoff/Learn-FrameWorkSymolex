ecdc09d437218938d1dacd04b12848830edacb1b
Vitaly Melnikov <vmelnikov@bk.ru>
2018-10-23 16:55:42 +0300

    Refactoring index.php

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php declare(strict_types=1);
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__.'/init.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
-
+/** @var \Symfony\Component\HttpFoundation\Request $request */
 $name = $request->get('name', 'World');
 
-$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
-
+$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
 $response->send();
