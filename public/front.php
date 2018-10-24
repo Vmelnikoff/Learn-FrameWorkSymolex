cf02c634c89423be2a3af8ecc8e102cc089da9fc
Vitaly Melnikov <vmelnikov@bk.ru>
2018-10-24 14:54:26 +0300

    Refactoring front.php for templating

--- public/front.php
+++ public/front.php
@@ -13,15 +13,21 @@ $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
     ob_start();
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
 
-    $response = new Response(ob_get_clean());
-} catch (Routing\Exception\ResourceNotFoundException $exception) {
+    return new Response(ob_get_clean());
+}
+
+try {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func('render_template', $request);
+} catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
-} catch (Exception $exception) {
+} catch (Exception $e) {
     $response = new Response('An error occurred', 500);
 }
 
