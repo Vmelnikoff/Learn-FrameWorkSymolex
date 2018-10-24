0ef82350cc585ef643aed2c9b70a736be0cba250
Vitaly Melnikov <vmelnikov@bk.ru>
2018-10-24 15:00:39 +0300

    Each route configured via the _controller route attribute

--- src/app.php
+++ src/app.php
@@ -3,7 +3,12 @@
 use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Routing\Route('/bye'));
+$routes->add('hello', new Routing\Route('/hello/{name}', [
+    'name' => 'World',
+    '_controller' => 'render_template',
+]));
+$routes->add('bye', new Routing\Route('/bye', [
+    '_controller' => 'render_template',
+]));
 
 return $routes;
